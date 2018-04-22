8fa47a2801cd36f8b938571bf62903ead247e0a5
sasaken555 <accelerk@gmail.com>
2018-04-22 23:06:09 +0900

    Deployment/Service作成関数を実装

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
 
 // ライブラリ
 const axios = require('axios');
+const AWS = require('aws-sdk');
+const yaml = require('js-yaml');
 
 // 定数
 const KUBE_BASE_URI = process.env.AWS_K8S_APISERVER;
 const KUBE_AUTH_TOKEN = process.env.AWS_K8S_TOKEN;
+const S3_KUBE_BUCKET = 'codepipeline-ap-northeast-1-208341755624';
+const S3_KUBE_REGION = 'ap-northeast-1';
+const s3 = new AWS.S3({ params: { Bucket: S3_KUBE_BUCKET, Region: S3_KUBE_REGION } });
 
 /**
  * デプロイ関数本体
@@ -19,13 +24,24 @@ const KUBE_AUTH_TOKEN = process.env.AWS_K8S_TOKEN;
  */
 exports.deployK8sDef = (event, context, callback) => {
   // Deploy Code here...
+  checkApi()
+  .then(getDeployments(namespace))
+  .then(getServices(namespace))
+  .then(deleteDeployment(namespace, obj_name))
+  .then(deleteService(namespace, obj_name))
+  .then(createDeployment(namespace))
+  .then(createService(namespace));
 }
 
-// exports.deployK8sDef = (namespace) => {
-//   // 非同期で実行されるため、順不同で表示される
+// exports.deployK8sDef = async (namespace, obj_name) => {
+//   // 非同期で実行されるため、順不同で表示される?
 //   checkApi()
 //     .then(getDeployments(namespace))
-//     .then(getServices(namespace));
+//     .then(getServices(namespace))
+//     .then(deleteDeployment(namespace, obj_name))
+//     .then(deleteService(namespace, obj_name))
+//     .then(createDeployment(namespace))
+//     .then(createService(namespace));
 // }
 
 const axiosInstance = axios.create({
@@ -43,7 +59,7 @@ const axiosInstance = axios.create({
  */
 async function checkApi() {
   console.log('Check API...');
-  return axiosInstance.get('/api')
+  await axiosInstance.get('/api')
           .then((res) => {
             console.log('API Info...');
             console.log(`Status: ${res.status} ${res.statusText}`);
@@ -57,25 +73,30 @@ async function checkApi() {
  */
 async function getDeployments(namespace) {
   console.log('Get Deployment List...');
-  return axiosInstance
+  await axiosInstance
           .get(`/apis/apps/v1/namespaces/${namespace}/deployments`)
           .then((res) => {
             console.log('Deployment List...');
             console.log(`Status: ${res.status} ${res.statusText}`);
 
-            res.data.items.forEach((deployItem, index, array) => {
-              console.log(`Name: ${deployItem.metadata.name}, Desired: ${deployItem.status.replicas}, Available: ${deployItem.status.availableReplicas}`);
-            });
-          });
+            if(res.data.items.length == 0) {
+              console.log('No resources found.');
+            }
+            else {
+              res.data.items.forEach((deployItem, index, array) => {
+                console.log(`Name: ${deployItem.metadata.name}, Desired: ${deployItem.status.replicas}, Available: ${deployItem.status.availableReplicas}`);
+              });
+            }
+        });
 }
 
 /**
  * Service一覧を取得する
  * @param {String} namespace 名前空間
  */
-function getServices(namespace) {
+async function getServices(namespace) {
   console.log('Get Services List...');
-  return axiosInstance
+  await axiosInstance
           .get(`/api/v1/namespaces/${namespace}/services`)
           .then((res) => {
             console.log('Services List...');
@@ -90,19 +111,44 @@ function getServices(namespace) {
 /**
  * Deploymentを作成する
  * @param {String} namespace 名前空間
- * @param {Object} manifest Deployment名
  */
-function createDeployment(namespace, manifest) {
-  // some code here
+function createDeployment(namespace) {
+  console.log('Get Deployment Manifest...');
+  const params = { Bucket: S3_KUBE_BUCKET, Key: 'sasaken-node-app-deployment.yml' };
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }
+    const manifest = yaml.safeLoad(new Buffer(data.Body).toString('UTF-8'), 'utf8');
+    return axiosInstance
+            .post(`/apis/apps/v1/namespaces/${namespace}/deployments`, manifest)
+            .then((res) => {
+              console.log('Create Deployment')
+              console.log(`Status: ${res.status} ${res.statusText}`);
+              console.log(`${res.data.metadata.name} * ${res.data.spec.replicas} created.`);
+            });
+  });
 }
 
 /**
  * Serviceを作成する
  * @param {String} namespace 名前空間
- * @param {Object} manifest Service名
  */
-function createService(namespace, manifest) {
-  // some code here
+function createService(namespace) {
+  console.log('Get Service Manifest...');
+  const params = { Bucket: S3_KUBE_BUCKET, Key: 'sasaken-node-app-service.yml' };
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }
+    const manifest = yaml.safeLoad(new Buffer(data.Body).toString('UTF-8'), 'utf8');
+    return axiosInstance
+            .post(`/api/v1/namespaces/${namespace}/services`, manifest)
+            .then((res) => {
+              console.log('Create Service')
+              console.log(`Status: ${res.status} ${res.statusText}`);
+            });
+  });
 }
 
 /**
@@ -110,17 +156,13 @@ function createService(namespace, manifest) {
  * @param {String} namespace 名前空間
  * @param {String} obj_name Deployment名
  */
-function deleteDeployment(namespace, obj_name) {
+async function deleteDeployment(namespace, obj_name) {
   console.log('Delete Deployments List...');
   return axiosInstance
-          .delete(`/api/v1/namespaces/${namespace}/deployments/${obj_name}`)
+          .delete(`/apis/apps/v1/namespaces/${namespace}/deployments/${obj_name}`)
           .then((res) => {
-            console.log('Deployments List...');
+            console.log('Delete Deployments...');
             console.log(`Status: ${res.status} ${res.statusText}`);
-
-            res.data.items.forEach((deployItem, index, array) => {
-              console.log(`Name: ${deployItem.metadata.name}, Desired: ${deployItem.status.replicas}, Available: ${deployItem.status.availableReplicas}`);
-            });
           });}
 
 /**
@@ -128,16 +170,12 @@ function deleteDeployment(namespace, obj_name) {
  * @param {String} namespace 名前空間
  * @param {String} obj_name Service名
  */
-function deleteService(namespace, obj_name) {
+async function deleteService(namespace, obj_name) {
   console.log('Delete Services List...');
   return axiosInstance
           .delete(`/api/v1/namespaces/${namespace}/services/${obj_name}`)
           .then((res) => {
-            console.log('Services List...');
+            console.log('Delete Services...');
             console.log(`Status: ${res.status} ${res.statusText}`);
-
-            res.data.items.forEach((serviceItem, index, array) => {
-              console.log(`Name: ${serviceItem.metadata.name}, Type: ${serviceItem.spec.type}, PortMappings: port:${serviceItem.spec.ports[0].port}-targetPort:${serviceItem.spec.ports[0].targetPort}`);
-            });
           });
   }
